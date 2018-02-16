dbb6e9060e725a47df13e7e5053a08c07b20da6f
bridmoynihan <35601773+bridmoynihan@users.noreply.github.com>
2018-02-16 11:46:34 +0000

    Update mortgage.js

--- public/scripts/mortgage.js
+++ public/scripts/mortgage.js
@@ -5,21 +5,20 @@ var loan = property.price/2;
 var transaction = true;
 //check if property is unimproved
 while(transaction){
-    if(property.houses == 0){
+    if(property.numberOfHouses == 0){
     //property is unimproved so sell to bank
         userObj.capital += loan;
-        property.mortgage = true;
+        property.mortgaged = true;
         var BaseRent = property.rent[0];
         window.BaseRent = BaseRent;
         property.rent[0] = 0;
         transation = false;
         }
     //otherwise property has improvements on it so remove and sell property
-    //property is sold at $50 regardless of what player originally paid for them. #HouseRule
     else{
-        while(property.houses != 0){
-            property.houses -=1;
-            userObj.capital += 50;
+        while(property.numberOfHouses != 0){
+            property.numberOfHouses -=1;
+            userObj.capital += property.houseValue/2;
             }
         }
     }
@@ -30,7 +29,7 @@ var loan = property.price/2;
 //calculate the cost of the loan
 var payBack = loan*.1 + loan;
 userObj.capital -= payBack;
-property.mortgage = false;
+property.mortgaged = false;
 property.rent[0] = BaseRent;
 
 }
