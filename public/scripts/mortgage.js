dc95e7649f5d5d2f3d0063f2d04a34667597a1de
bridmoynihan <35601773+bridmoynihan@users.noreply.github.com>
2018-02-20 13:35:03 +0000

    Update mortgage.js

--- public/scripts/mortgage.js
+++ public/scripts/mortgage.js
@@ -1,37 +1,37 @@
 //This function is always an available option to the player
 //if player wants to call this function they must pass their property of interest as an explicit parameter.
 //only properties which the player owns deeds to should be available for mortgage.
-function mortgage(property){
+function mortgage(userObject, properties){
 //calculate the loan amount
-var loan = property.price/2;
+var loan = properties.price/2;
 var transaction = true;
 //check if property is unimproved
 while(transaction){
-    if(property.numberOfHouses == 0){
+    if(properties.numberOfHouses == 0){
     //property is unimproved so sell to bank
-        userObj.capital += loan;
-        property.mortgaged = true;
-        var BaseRent = property.rent[0];
+        userObject.capital += loan;
+        properties.mortgaged = true;
+        var BaseRent = properties.rent[0];
         window.BaseRent = BaseRent;
-        property.rent[0] = 0;
+        properties.rent[0] = 0;
         transation = false;
         }
     //otherwise property has improvements on it so remove and sell property
     else{
-        while(property.numberOfHouses != 0){
-            property.numberOfHouses -=1;
-            userObj.capital += property.houseValue/2;
+        while(properties.numberOfHouses != 0){
+            properties.numberOfHouses -=1;
+            userObj.capital += properties.houseValue/2;
             }
         }
     }
 }
-function PayOffLoan(property){
+function PayOffLoan(userObject, properties){
 //calculate the loan amount
-var loan = property.price/2;
+var loan = properties.price/2;
 //calculate the cost of the loan
 var payBack = loan*.1 + loan;
-userObj.capital -= payBack;
-property.mortgaged = false;
-property.rent[0] = BaseRent;
+userObject.capital -= payBack;
+properties.mortgaged = false;
+properties.rent[0] = BaseRent;
 
 }
