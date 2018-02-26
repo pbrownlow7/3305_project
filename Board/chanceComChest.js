9bd5afb30a9c68ddb5aaae875a85bd2e5dbabd30
David Donnellan <35601793+davidjdonnellan@users.noreply.github.com>
2018-02-26 11:43:25 +0000

    Add files via upload

--- Board/chanceComChest.js
+++ Board/chanceComChest.js
@@ -6,7 +6,6 @@ function checkTile(playerPos) {
     } else if(playerPos == "0007" || playerPos == "1008" || playerPos == "0400") {
         //Player has landed on chance card
         //console.log("Draw chance card");
-        //shuffle();
         alert("Draw chance card");
         chance(playerPos, player);
     } else if(playerPos == "0002" || playerPos == "0710" || playerPos == "0700") {
@@ -39,12 +38,12 @@ var chanceArray = [
     },
 
     {
-    Name: "Go directly to Jail – Do not pass Go, do not collect €200",
+    Name: "Go directly to Jail. Do not pass Go, do not collect 200",
     Id: 8,
     },
 
     {
-    Name: "Make general repairs on all your property. For each house pay €25. For each hotel €100",
+    Name: "Make general repairs on all your property. For each house pay 25. For each hotel 100",
     Id: 9,
     Amount: [25, 100] //25 is per house and 100 is per hotel
     },
@@ -76,25 +75,25 @@ var chanceArray = [
     },
 
     {
-    Name: "Advance to Free Parking – If you pass Go, collect €200",
+    Name: "Advance to Free Parking. If you pass Go, collect 200",
     Id: 18,
     Tile: "1010"
     },
 
     {
-    Name: "Advance to HillBillys – If you pass Go, collect €200",
+    Name: "Advance to HillBillys. If you pass Go, collect 200",
     Id: 19,
     Tile: "1006"
     },
 
     {
-    Name: "Advance to Go (Collect €200)",
+    Name: "Advance to Go (Collect 200)",
     Id: 20,
     Tile: "0000"
     },
 
     {
-    Name:"Advance to Castle White – If you pass Go, collect €200",
+    Name:"Advance to Castle White If you pass Go, collect 200",
     Id: 21,
     Tile: "0110"
     },
@@ -107,7 +106,7 @@ var chanceArray = [
 ]
 var communityChestArray = [
     {
-      Name: "You are assessed for street repairs. €40 per house, €115 per hotel",
+      Name: "You are assessed for street repairs. 40 per house, 115 per hotel",
       Id: 9,
       Amount: [40, 115] //40 is per house and 115 is per hotel
     },
@@ -119,24 +118,24 @@ var communityChestArray = [
     },
 
     {
-        Name: "Doctor's fees. Pay €50",
+        Name: "Doctor's fees. Pay 50",
         Id: 2,
         Amount: 50
     },
 
     {
-        Name:"From sale of stock you get €50",
+        Name:"From sale of stock you get 50",
         Id: 3,
         Amount: 50
     },
     {
-        Name: "Grand Opera Night, Collect €50 from every player for opening night seats",
+        Name: "Grand Opera Night, Collect 50 from every player for opening night seats",
         Id: 4,
         Amount: 50
     },
 
     {
-        Name: "Holiday Fund matures. Receive €100",
+        Name: "Holiday Fund matures. Receive 100",
         Id: 5,
         Amount: 100
     },
@@ -147,124 +146,154 @@ var communityChestArray = [
     },
 
     {
-        Name: "Income tax refund. Collect €25",
+        Name: "Income tax refund. Collect 25",
         Id: 7,
         Amount: 25
     },
 
     {
-        Name: " Go directly to jail. Do not pass Go. Do not collect €200",
+        Name: " Go directly to jail. Do not pass Go. Do not collect 200",
         Id: 8,
     },
 
 
     {
-        Name: "It is your birthday Collect €10 from each player ",
+        Name: "It is your birthday Collect 10 from each player ",
         Id: 10,
         Amount: 10
     },
 
     {
-        Name: "Life insurance matures Collect €100",
+        Name: "Life insurance matures Collect 100",
         Id: 11,
         Amount: 100
     },
 
     {
-        Name: "Pay hospital fees of €100",
+        Name: "Pay hospital fees of 100",
         Id: 12,
         Amount: 100
     },
 
     {
-        Name: "Pay school fees of €150",
+        Name: "Pay school fees of 150",
         Id: 13,
         Amount: 150
     }
   ]
 
 function chance(playerPos,player){
-  var card = chanceArray.shift() // takes top card from array
-  chanceArray.push(card); // adds card to end of array
-
-  if (card.Id == 6 ){
+ var card = chanceArray.shift() // takes top card from array
+ chanceArray.push(card); // adds card to end of array
+ if (card.Id == 6 ){
      setJailCard();
  }
-
  else if (card.Id == 20 ){
-      alert(card.Name);
+      alert(card.Name); // advance to Go
       advance(card.Tile);
+      players[turn].position = card.Tile;
+      collect200();
  }
-
  else if (card.Id == 8){
    alert(card.Name);
    placeInJail();
-
  }
  else if(card.Id== 19){
    // add go functionality
-    alert(card.Name);
-    advance(card.Tile);
+   alert(card.Name);  // Hillbillys  Done
+   if (players[turn].position == "0400"){
+     collect200();
+   }
+   advance(card.Tile);
+   players[turn].position = card.Tile;
+   isOwned(players[turn],card.Tile);
  }
  else if(card.Id == 21){
-   // add go functionality
+   // add go functionality Castle White Done
    alert(card.Name);
+   if (players[turn].position == "0400" ||players[turn].position == "1008"){
+     collect200();
+   }
    advance(card.Tile);
+   players[turn].position = card.Tile;
+   isOwned(players[turn],card.Tile);
  }
- else if (card.Id == 17){
+ else if (card.Id == 17){ //wgb
    alert(card.Name);
    advance(card.Tile);
+   players[turn].position = card.Tile;
+   isOwned(players[turn],card.Tile);
  }
  else if(card.Id == 18){
    // add go functionality
    alert(card.Name);
-   advance(card.Tile); //  advances player to specific tile
+   if (players[turn].position == "0400" ||players[turn].position == "1008"){
+     collect200();
+   }
+   advance(card.Tile); //  advances player to Free Parking Done
  }
-
  else if(card.Id == 15){
     alert(card.Name)
     movePlayer(players[turn], 3, turn); // moves player forward 3 spaces
  }
  else if (card.Id == 16){
    alert(card.Name)
-   //movePlayer(player, 37, turn); need to create function that moves player back
+   //movePlayerBack();
  }
  else if (card.Id == 9 ){
    // must calculate the amount of houses and hotels the player has.
    alert(card.Name);
    calcHouseHotels(card.Amount[0],card.Amount[1]); // calculates players houses and hotels
-}
-
+ }
  else if (card.Id == 14) {
    //need to add collect 200 as go was passed
    alert(card.Name);
 
    if(playerPos == "0007"){
-     document.getElementById("0510").appendChild(players[turn].id);
+     document.getElementById("0510").appendChild(players[turn].id); //advances to Cork Airport
+     players[turn].position = "0510";
+     isOwned(players[turn],"0510");
    }
    else if(playerPos == "1008"){
-     document.getElementById("1005").appendChild(players[turn].id);
-
+     document.getElementById("1005").appendChild(players[turn].id); // advances to Bus station
+     players[turn].position = "0510";
+     isOwned(players[turn],"0510");
    }
    else if(playerPos == "0400"){
-     document.getElementById("0005").appendChild(players[turn].id);
+     document.getElementById("0005").appendChild(players[turn].id); // advances taxi rank
+     players[turn].position = "0510";
+     isOwned(players[turn],"0510");
+     collect200(); // collect 200 as player passed go
    }
  }
  else if (card.Id == 22) {
-   //need to add collect 200 as go was passed
+   // advances player to nearest utility
    alert(card.Name);
 
    if(playerPos == "0007"){
      document.getElementById("0210").appendChild(players[turn].id);
+     players[turn].position = "0210";
+     isOwned(players[turn], "0210");
    }
    else if(playerPos == "1008"){
      document.getElementById("1002").appendChild(players[turn].id);
+     players[turn].position = "1002";
+     isOwned(players[turn],"1002");
    }
    else if(playerPos == "0400"){
-     // collectGo()
      document.getElementById("0210").appendChild(players[turn].id);
+     players[turn].position = "0210";
+     isOwned(players[turn],"0210");
+     collect200();
    }
  }
+}
+//function movePlayerBack(){}
+
+function collect200(){
+  players[turn].money += 200;
+  alert("Player Passed Go. Collect 200");
+  console.log(players[turn].money);
 }
 
 function communityChest(playerPos, player){
@@ -364,15 +393,15 @@ function playerCollect(amount){
   console.log(players);
 }
 
-function shuffles(){
+function shuffles(array){
   var i =0;
   var j = 0;
   var temp = null;
 
-  for(i = chanceArray.length-1;i>0;i-=1){
+  for(i = array.length-1;i>0;i-=1){
       j = Math.floor(Math.random()*(i+1));
-      temp=chanceArray[i];
-      chanceArray[i]=chanceArray[j];
-      chanceArray[j]=temp;
+      temp=array[i];
+      array[i]=array[j];
+      array[j]=temp;
   }
 }
